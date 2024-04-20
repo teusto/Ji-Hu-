b1ea23f7ba4008c1250a25ba5f9bfa45938b4737
Matheus Toscano <pteutoscano@gmail.com>
2024-04-20 15:23:24 +0100

    feat: grainy effect to blur video background

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Loader from './components/Loader';
 import BusinessCard from './components/BusinessCard';
 import MainWrapper from './components/Main';
+import './style.scss';
 
 const App = (): React.JSX.Element => {
     return (
         <>
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
                 <BusinessCard />
+                <div className='Video--blur'></div>
                 <MainWrapper />
             </div>
         </>
