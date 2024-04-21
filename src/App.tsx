4054fb39952b4ededd39f74945f546b8b7246882
Matheus Toscano <pteutoscano@gmail.com>
2024-04-21 02:11:01 +0100

    feat: animated loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Loader from './components/Loader';
 import BusinessCard from './components/BusinessCard';
 import MainWrapper from './components/Main';
 import './style.scss';
 
 const App = (): React.JSX.Element => {
+    const [showLoader, setShowLoader] = useState(true);
+
+    useEffect(() => {
+        // Use setTimeout to update the message after 5000 milliseconds (5 seconds)
+        const timeoutId = setTimeout(() => {
+          setShowLoader(false);
+        }, 5000);
+    
+        // Cleanup function to clear the timeout if the component unmounts
+        return () => clearTimeout(timeoutId);
+      }, []);
+
     return (
         <>
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+                {showLoader && <Loader />}
                 <BusinessCard />
                 <div className='Video--blur'></div>
                 <MainWrapper />
--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import './style.scss';
+import { motion, useAnimate, stagger } from "framer-motion";
 
 const Loader = (): React.JSX.Element => {
-    return(
-        <div>loader page</div>
+    const [scope, animate] = useAnimate();
+    let sentence1: any = Array.from('Impressive,\xa0very\xa0nice\xa0...');
+    let sentence2: any = Array.from(`Let's\xa0see\xa0Matheus\xa0Toscano\xa0card`);
+
+    useEffect(() => {
+        animate("p", { opacity: 1 }, { ease: "linear", delay: stagger(.05) })
+    }, [scope])
+
+    return (
+        <div className='Loader__wrapper'>
+            <motion.div className='Loader__top' animate={{ x: '-100%' }} transition={{ delay: 3.5, duration: 1.2 }}>
+                <div ref={scope} className='__top__text'>
+                    <div className='__text__sentence'>
+                        {sentence1.map(l => <p className='--text'>{l}</p>)}
+                    </div>
+                    <div className='__text__sentence'>
+                        {sentence2.map(l => <p className='--text'>{l}</p>)}
+                    </div>
+                </div>
+            </motion.div>
+            <motion.div className='Loader__bottom' animate={{ x: '100%' }} transition={{ delay: 3.5, duration: 1.2 }}></motion.div>
+        </div>
     )
 }
 
